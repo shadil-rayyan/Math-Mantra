81e06a9f8146acf046c53eb3dee954794c963b2c
shadil-rayyan <shadilrayyan2@gmail.com>
2025-08-05 23:07:09 +0530

    fixed the warning of diffcultypreference file

--- app/src/main/java/com/zendalona/zmantra/presentation/features/setting/util/DifficultyPreferences.kt
+++ app/src/main/java/com/zendalona/zmantra/presentation/features/setting/util/DifficultyPreferences.kt
@@ -1,18 +1,25 @@
 package com.zendalona.zmantra.presentation.features.setting.util
 
 import android.content.Context
-import android.preference.PreferenceManager
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
 
 object DifficultyPreferences {
     private const val KEY_DIFFICULTY = "pref_difficulty_level"
 
     fun setDifficulty(context: Context, level: Int) {
         val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-        prefs.edit().putInt(KEY_DIFFICULTY, level).apply()
+        prefs.edit() { putInt(KEY_DIFFICULTY, level) }
     }
 
+    // This function accepts a nullable Context and handles the null case
     fun getDifficulty(context: Context?): Int {
+        if (context == null) {
+            return 1 // Return a default value if the context is null
+        }
         val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-        return prefs.getInt(KEY_DIFFICULTY, 1) // default is SIMPLE = 1
+        return prefs.getInt(KEY_DIFFICULTY, 1)
     }
+
 }
+
