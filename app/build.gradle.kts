a0fb2a0b24bc70f200f5ebd64b7bcf6c602ad59e
shadil-rayyan <shadilrayyan2@gmail.com>
2025-08-05 18:29:04 +0530

    feat(build.grdle):move the harded code verison to lib.version.toml file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.kotlin.dsl.debugImplementation
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -11,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.zendalona.zmantra"
-        minSdk = 26
+        minSdk = 30
         targetSdk = 35
         versionCode = 3
         versionName = "1.0.9 "
@@ -54,50 +52,50 @@ dependencies {
     implementation(libs.androidx.fragment)
 
     // Third-party Libraries
-    implementation("org.apache.poi:poi:5.2.3")
-    implementation("org.apache.poi:poi-ooxml:5.2.3")
+    implementation(libs.poi)
+    implementation(libs.poi.ooxml)
     implementation(libs.lottie)
     implementation(libs.glide)
     annotationProcessor(libs.compiler)
     implementation(libs.exp4j)
 
     // Testing Dependencies
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1") // Optional
-    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test) // Optional
+    testImplementation(libs.androidx.core.testing)
 
     // Android Instrumentation Tests
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("org.mockito:mockito-android:4.11.0")
-    debugImplementation("androidx.fragment:fragment-testing:1.5.7")
+    androidTestImplementation(libs.androidx.espresso.core.v351)
+    androidTestImplementation(libs.androidx.junit.v115)
+    androidTestImplementation(libs.androidx.rules)
+    androidTestImplementation(libs.mockito.android)
+    debugImplementation(libs.androidx.fragment.testing)
 
     // Espresso UI Testing
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1") // Espresso core
-    androidTestImplementation("androidx.test.espresso:espresso-idling-resource:3.5.1") // Optional for handling idle resources
+    androidTestImplementation(libs.androidx.espresso.core.v351) // Espresso core
+    androidTestImplementation(libs.androidx.espresso.idling.resource)
 
     // JUnit4 Support for running tests
-    androidTestImplementation("androidx.test.ext:junit:1.1.5") // JUnit4 for Android
+    androidTestImplementation(libs.androidx.junit.v130) // JUnit4 for Android
 
     // Test Runner for Android tests
-    androidTestImplementation("androidx.test:runner:1.5.2") // Test runner for Android tests
+    androidTestImplementation(libs.androidx.runner) // Test runner for Android tests
 
     // Fragment Testing APIs
-    debugImplementation("androidx.fragment:fragment-testing:1.5.5") // For fragment-related testing
+    debugImplementation(libs.androidx.fragment.testing.v155) // For fragment-related testing
 
     // To launch fragments in test
-    debugImplementation("androidx.fragment:fragment-testing:1.5.5") // Fragment testing APIs
+    debugImplementation(libs.androidx.fragment.testing) // Fragment testing APIs
 
     // Lifecycle support for testing lifecycle-related aspects of fragments
-    androidTestImplementation("androidx.lifecycle:lifecycle-runtime-testing:2.5.1") // For lifecycle testing
+    androidTestImplementation(libs.androidx.lifecycle.runtime.testing) // For lifecycle testing
 
     // Core Test library for Android
-    androidTestImplementation("androidx.test:core:1.5.0") // Core functionality for AndroidX testing
+    androidTestImplementation(libs.androidx.core) // Core functionality for AndroidX testing
 
 
     // Optional for mocking the navigation component (if you use NavController or a similar component)
-    androidTestImplementation("androidx.navigation:navigation-testing:2.5.3") // Navigation testing support
-    debugImplementation ("com.squareup.leakcanary:leakcanary-android:2.13")
+    androidTestImplementation(libs.androidx.navigation.testing) // Navigation testing support
+    debugImplementation (libs.leakcanary.android)
 
 }
